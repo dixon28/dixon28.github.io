55dcf22688707a258ce1356210c111ab64d1009f
dixon28 <dxam15005@gmail.com>
2021-08-07 16:56:17 -0600

    limpiando

--- bad115_planilla/bad115_planilla-master/SistemaPlanilla/app/Views/plantilla/footer.php
+++
@@ -1,7 +0,0 @@
-<footer class="main-footer">
-    <strong><?= $grupo?></strong>
-    
-    <div class="float-right d-none d-sm-inline-block">
-      <b><?= $materia?></b>
-    </div>
-</footer>
--- bad115_planilla/bad115_planilla-master/SistemaPlanilla/public/js/generar_planilla/generar_planilla.js
+++
@@ -1,2 +0,0 @@
-
-$('#t_planilla_paginate').css('display', 'none');
